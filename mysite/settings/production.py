5f13176b1ace69dd35ce891428c13103f1cd9328
Piotr Delikat <piotrdellikat@gmail.com>
2017-08-12 13:10:06 +0200

    Changed production db to mysql

---
+++ mysite/settings/production.py
@@ -0,0 +1,11 @@
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_site_db',
+        'USER': 'username',
+        'PASSWORD': 'password',
+    }
+}
