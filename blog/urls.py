08b673060b569e8fe366a7a6e55ad04f0ec9bddf
Piotr Delikat <piotrdellikat@gmail.com>
2017-08-17 16:38:22 +0200

    Basic url structure and home

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import DetailView
+from .models import *
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list')
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model= Post, template_name="blog/post.html"))
 ]
 
