7dace728789dcacfd1d405d1629ec1a4dfacbd5f
Piotr Delikat <piotrdellikat@gmail.com>
2017-09-18 09:45:45 +0200

    Contact text, link

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
-    url(r'^privacy-policy/$', views.privacy, name='privacy')
+    url(r'^privacy-policy/$', views.privacy, name='privacy'),
+    url(r'^frankentie_promo/index.html', views.promo, name='promo')
 ]
 
