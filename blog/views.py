63420fb809c034e7c09f1b33fa4c5f167d4402d1
Piotr Delikat <piotrdellikat@gmail.com>
2017-12-22 17:49:53 +0100

    Defined function for query the blog post detail view by slug

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-# Create your views here.
+from django.http import HttpResponse
+
+
 def post_list(request):
     posts = Post.objects.order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'blog/post.html', {'post':post})
